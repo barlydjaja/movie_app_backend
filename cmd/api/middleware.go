4211363f540706a0e0573e191c2bbd880fab7114
barly.djaja <barly.djaja@rupiahcepat.co.id>
2022-04-06 09:14:21 +0700

    update

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,7 +6,7 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 		next.ServeHTTP(w, r)
 	})
 }
